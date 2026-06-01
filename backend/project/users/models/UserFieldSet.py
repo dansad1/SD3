791e09f2a1317d821f15b7a4df6a637f2c4fbfa8
dansad1 <dansadkov@yandex.ru>
2026-06-01 18:06:21 +0300

    layout форм

--- backend/project/users/models/UserFieldSet.py
+++ backend/project/users/models/UserFieldSet.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 from backend.generic.models.BaseFieldSet import BaseFieldSet
+from backend.project.companies.models import CompanyFieldSet
+from backend.project.tickets.models import TicketFieldSet
 
 
 class UserFieldSet(BaseFieldSet):
@@ -14,3 +16,6 @@ class UserFieldSet(BaseFieldSet):
 
     def __str__(self):
         return self.name
+
+
+
